fdbe590172d3b07892acb5b172d0fea8fbc1f101
Cristian.x <cristian.x.moreno.perez@gmail.com>
2026-02-16 01:10:59 -0500

    chore: inicializa proyecto Angular 21 con configuración base

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,25 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { providePrimeNG } from 'primeng/config';
 import { routes } from './app.routes';
+import Aura from '@primeuix/themes/aura';
+import { ConfirmationService, MessageService } from 'primeng/api';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: 'light',
+        },
+      },
+    }),
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    MessageService,
+    ConfirmationService,
+  ],
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,35 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { ToastModule } from 'primeng/toast';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { MessagesService } from './core/services/messages.service';
+import { ConfirmationsService } from '@core/services/confirmation.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule, ToastModule, ConfirmDialogModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
+  private readonly messageService = inject(MessagesService);
+  private readonly confirmationService = inject(ConfirmationsService);
+
   protected readonly title = signal('school-app');
+
+
+
+  confirm() {
+    this.confirmationService.confirmation({
+      message: 'Are you sure that you want to proceed?',
+      header: 'Confirmation',
+      accept: () => {
+        this.messageService.showSuccess('Confirmed', 'You have accepted');
+      },
+      reject: () => {
+        this.messageService.showError('Rejected', 'You have rejected');
+      },
+    });
+  }
 }
